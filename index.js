ef4d3fe79b8df45a493bd319ad2949ca4b792a31
theodorepk <theodore.perron.kyritsos@gmail.com>
2022-08-17 22:28:12 +0200

    first route totaly working

--- index.js
+++ index.js
@@ -2,17 +2,30 @@ require(`dotenv`).config();
 const express = require("express");
 const app = express();
 const axios = require(`axios`);
+const { log } = require("console");
+const { resourceLimits } = require("worker_threads");
 
+//Just some test MUST CHANGE
 app.get(`/`, (req, res) => {
+  console.log(req.query);
   res.json({ message: "Hi" });
 });
 
+//Route .get who request the /comics route from Reacteur Marvel API
 app.get(`/comics`, async (req, res) => {
+  // Store the request on const variable with destructuring method
+  const { limit, skip, title } = req.query;
+  console.log(`limit is ${limit}, skip is ${skip} and title is ${title}`);
   try {
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}`
+      //.env marvel api key doesn't push to gitHub (cf Gitignore)
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${
+        process.env.MARVEL_API_KEY
+        //Here ternary avoid to send undefined variable to /comics route, send empty string instead
+      }&title=${title ? title : ``}&limit=${limit ? limit : ``}&skip=${
+        skip ? skip : ``
+      }`
     );
-    console.log(response.data);
     res.status(200).json(response.data);
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -20,6 +33,7 @@ app.get(`/comics`, async (req, res) => {
   }
 });
 
+// prise en compte des autres routes
 app.all(`*`, (req, res) => {
   res.status(404).json({ message: `Page not found` });
 });
