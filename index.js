431af386656d091ceac2ac6e3aecd9bc904995c4
theodorepk <theodore.perron.kyritsos@gmail.com>
2022-08-17 23:13:08 +0200

    characters route working

--- index.js
+++ index.js
@@ -11,19 +11,39 @@ app.get(`/`, (req, res) => {
   res.json({ message: "Hi" });
 });
 
-//Route .get who request the /comics route from Reacteur Marvel API
+// Route: /comics
+// It ask the Reacteur Marvel API with query parameters. Only limit, skip and title parameters will be send to the /comic route from Marvel API
 app.get(`/comics`, async (req, res) => {
-  // Store the request on const variable with destructuring method
-  const { limit, skip, title } = req.query;
-  console.log(`limit is ${limit}, skip is ${skip} and title is ${title}`);
+  const { limit, skip, title } = req.query; // Store the request on const variable with destructuring method
   try {
     const response = await axios.get(
-      //.env marvel api key doesn't push to gitHub (cf Gitignore)
       `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${
-        process.env.MARVEL_API_KEY
-        //Here ternary avoid to send undefined variable to /comics route, send empty string instead
+        process.env.MARVEL_API_KEY //.env marvel api key doesn't push to gitHub (cf Gitignore)
       }&title=${title ? title : ``}&limit=${limit ? limit : ``}&skip=${
-        skip ? skip : ``
+        skip && skip //Ternary avoid to send undefined variable to /comics route, send empty string instead
+      }`
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+    console.log(error.message);
+  }
+});
+
+// Route: /comics/:characterId
+
+app.get(`/comics/:charactersId`);
+
+// Route: /characters
+
+app.get(`/characters`, async (req, res) => {
+  const { limit, skip, name } = req.query;
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${
+        process.env.MARVEL_API_KEY
+      }&name=${name ? name : ``}&limit=${limit ? limit : ``}&skip=${
+        skip ? skip : `` //Ternary avoid to send undefined variable to /comics route, send empty string instead
       }`
     );
     res.status(200).json(response.data);
